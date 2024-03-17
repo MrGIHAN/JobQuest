21a94a83c8f104499fa6d57079af0fe3dfb02f5b
Chamod Gihan Pathirana <gihanpathirana84@gmail.com>
2024-03-17 23:16:40 +0530

    backend update

--- Back-End/server.js
+++ Back-End/server.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const URI = 'mongodb+srv://jobapp:jobapp@cluster0.awhxm2z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const URI = 'mongodb+srv://admin:@cluster0.ogorx9a.mongodb.net/?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3080;
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useFindAndModify: false
 }).then(() => {
     console.log("Database connection is successful!");
 }).catch((err) => {
-    console.log("Database connection is unsuccessful", err);
+    console.error("Database connection failed:", err);
 });
 
 app.listen(PORT, () => {
